111f65beafacce432679bb2ada5d26228d253dec
evayehct10 <77874297+evayehct10@users.noreply.github.com>
2021-07-01 00:18:53 -0700

    revised 3rd commit in new branch

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 console.log("In new branch")
+<<<<<<< HEAD
 
 console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("That is weird!")
@@ -6,3 +7,7 @@ console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+=======
+console.log("Add some aother to try if another commit will let test.js appear twice in pull request
+"
+>>>>>>> 98d54a1 (3rd commit in new branch)
