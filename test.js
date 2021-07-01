bd9dc095bffafd56ac14a6fd145ddb6029ac1caf
evayehct10 <77874297+evayehct10@users.noreply.github.com>
2021-07-01 15:38:15 -0700

    change squash commit 5 & 2 - modified01
    5th commit in new

--- test.js
+++ test.js
@@ -1,36 +1,19 @@
 console.log("In new branch")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 1a72782 (squash commit 5 & 2)
 console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("That is weird!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request
-"
->>>>>>> 98d54a1 (3rd commit in new branch)
-=======
+
+console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("That is weird!")
->>>>>>> 5ecb0f2 (4th commit in new branch)
-=======
->>>>>>> 93744ab (5th commit in new)
-=======
->>>>>>> 1a72782 (squash commit 5 & 2)
-=======
 
->>>>>>> 1f39bba (1st commit in new branch)
-=======
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request
-"
->>>>>>> 98d54a1 (3rd commit in new branch)
+console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
+console.log("That is weird!")
+console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
