152b52ff60e0048049b6ad331d3d4624a40891cb
evayehct10 <77874297+evayehct10@users.noreply.github.com>
2021-06-30 23:51:56 -0700

    change squash commit 5 & 2

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 console.log("In new branch")
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> 1a72782 (squash commit 5 & 2)
 console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("That is weird!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 console.log("Add some aother to try if another commit will let test.js appear twice in pull request
@@ -19,3 +23,5 @@ console.log("That is weird!")
 >>>>>>> 5ecb0f2 (4th commit in new branch)
 =======
 >>>>>>> 93744ab (5th commit in new)
+=======
+>>>>>>> 1a72782 (squash commit 5 & 2)
