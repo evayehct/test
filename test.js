d10b6e7714c6f1373dca5394c42f1b936bd8946a
evayehct10 <77874297+evayehct10@users.noreply.github.com>
2021-07-01 00:18:53 -0700

    revised - 3rd commit in new branch

--- test.js
+++ test.js
@@ -5,3 +5,4 @@ console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+console.log("Add some aother to try if another commit will let test.js appear twice in pull request
