e7d37fc586dc71a2b4533044b664ec9f993c73eb
evayehct10 <77874297+evayehct10@users.noreply.github.com>
2021-07-01 22:51:18 -0700

    squash commit 5 & 2 - modified

--- test.js
+++ test.js
@@ -1,17 +1,5 @@
 console.log("In new branch")
 
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
-console.log("That is weird!")
-console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
-console.log("That is weird!")
-
-console.log("Add some aother to try if another commit will let test.js appear twice in pull request")
 console.log("That is weird!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 console.log("That is weird!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
